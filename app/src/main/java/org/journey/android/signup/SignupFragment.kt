df7c34f9a40bdbece2c39c76475d4d69bf336e19
JooYae <jooyae3970@naver.com>
2021-06-30 02:00:03 +0900

    #5 Mainfragment View

---
+++ app/src/main/java/org/journey/android/signup/SignupFragment.kt
@@ -0,0 +1,13 @@
+package org.journey.android.signup
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class SignupFragment : Fragment() {
+   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+      super.onViewCreated(view, savedInstanceState)
+   }
+}
