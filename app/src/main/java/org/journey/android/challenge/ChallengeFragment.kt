64810b1d760b55566f11d3827140af5101940e58
JooYae <jooyae3970@naver.com>
2021-06-30 19:46:54 +0900

    [feat] #8 Viewpager2 with Bottomnavigation

---
+++ app/src/main/java/org/journey/android/challenge/ChallengeFragment.kt
@@ -0,0 +1,19 @@
+package org.journey.android.challenge
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import org.journey.android.R
+
+class ChallengeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_challenge, container, false)
+    }
+}
