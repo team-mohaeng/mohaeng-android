ccdd722c68f4118d5966ce80ba273d0a71c444d1
JooYae <jooyae3970@naver.com>
2021-07-02 18:09:17 +0900

    [fix] #18 Fix Journey Community

--- app/src/main/java/org/journey/android/community/CommunityFragment.kt
+++ app/src/main/java/org/journey/android/community/CommunityFragment.kt
@@ -6,15 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import org.journey.android.R
+import org.journey.android.base.BaseFragment
+import org.journey.android.databinding.FragmentCommunityBinding
 
-class CommunityFragment : Fragment() {
+class CommunityFragment : BaseFragment<FragmentCommunityBinding>() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_community, container, false)
+    override fun getFragmentBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ): FragmentCommunityBinding {
+        return FragmentCommunityBinding.inflate(inflater, container, false)
     }
+
 
 }
