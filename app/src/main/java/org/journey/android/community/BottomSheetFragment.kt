d69b9e78d94879c164c29695874d1dee4b959fb3
JooYae <jooyae3970@naver.com>
2021-07-05 18:51:58 +0900

    [feat] #34 Custom Journey Record

--- app/src/main/java/org/journey/android/community/BottomSheetFragment.kt
+++ app/src/main/java/org/journey/android/community/BottomSheetFragment.kt
@@ -5,18 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import org.journey.android.R
 import org.journey.android.base.BaseFragment
 import org.journey.android.databinding.FragmentBottomSheetBinding
 
-class BottomSheetFragment : BaseFragment<FragmentBottomSheetBinding>() {
-    override fun getFragmentBinding(
-        inflater: LayoutInflater,
-        container: ViewGroup?
-    ): FragmentBottomSheetBinding {
-        return FragmentBottomSheetBinding.inflate(inflater, container, false)
-    }
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-    }
+class BottomSheetFragment : BottomSheetDialogFragment() {
 }
