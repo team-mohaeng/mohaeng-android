88c103d3e4ef7cf865e7ae30c9329fd7a0cda630
JooYae <jooyae3970@naver.com>
2021-07-06 22:25:19 +0900

    [feat] #40 Journey Reward UI

---
+++ app/src/main/java/org/journey/android/reward/RewardAdapter.kt
@@ -0,0 +1,4 @@
+package org.journey.android.reward
+
+class RewardAdapter {
+}
---
+++ app/src/main/java/org/journey/android/reward/RewardFragment.kt
@@ -0,0 +1,26 @@
+package org.journey.android.reward
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import org.journey.android.base.BaseFragment
+import org.journey.android.databinding.FragmentRewardBinding
+
+class RewardFragment : BaseFragment<FragmentRewardBinding>() {
+
+    override fun getFragmentBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ): FragmentRewardBinding {
+      return FragmentRewardBinding.inflate(inflater, container,false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+
+}
