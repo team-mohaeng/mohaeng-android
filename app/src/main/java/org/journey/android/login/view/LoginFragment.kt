8e5dc2b145fd33316bbed73c2d6427e14e7504af
JooYae <jooyae3970@naver.com>
2021-07-13 05:39:06 +0900

    [feat] #123 Modify ViewBinding Login

--- app/src/main/java/org/journey/android/login/view/LoginFragment.kt
+++ app/src/main/java/org/journey/android/login/view/LoginFragment.kt
@@ -19,7 +19,7 @@ import org.journey.android.databinding.FragmentLoginBinding
 import org.journey.android.databinding.FragmentSplashBinding
 
 class LoginFragment : Fragment() {
-    lateinit var binding : FragmentLoginBinding
+    lateinit var binding: FragmentLoginBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,56 +28,15 @@ class LoginFragment : Fragment() {
     ): View? {
         binding = FragmentLoginBinding.inflate(inflater, container, false)
         return binding.root
-
-//        val loginView = inflater.inflate(R.layout.fragment_login, null)
-//
-//        val edittextInputEmail = loginView.findViewById(R.id.edittext_login_email) as EditText
-//        val edittextInputPassword = loginView.findViewById(R.id.edittext_login_password) as EditText
-//        val buttonDeleteEmail = loginView.findViewById(R.id.button_delete_email)as AppCompatButton
-//        val buttonDeletePassword = loginView.findViewById(R.id.button_delete_password)as AppCompatButton
-//
-//        binding.edittextLoginEmail.addTextChangedListener(object: TextWatcher {
-//
-//            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-//            }
-//            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-//               if(edittextInputEmail.text.toString().length>0)
-//                   buttonDeleteEmail.isVisible=true
-//            }
-//            override fun afterTextChanged(s: Editable?) {
-//            }
-//        })
-//        binding.edittextLoginPassword.addTextChangedListener(object: TextWatcher {
-//
-//            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-//            }
-//            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-//                if(edittextInputPassword.text.toString().length>0)
-//                    buttonDeletePassword.isVisible=true
-//            }
-//            override fun afterTextChanged(s: Editable?) {
-//            }
-//        })
-//
-//        binding.buttonDeleteEmail.setOnClickListener{
-//            edittextInputEmail.setText("")
-//            buttonDeleteEmail.isVisible=false
-//        }
-//
-//        binding.buttonDeletePassword.setOnClickListener{
-//            edittextInputPassword.setText("")
-//            buttonDeletePassword.isVisible=false
-//        }
-//
-//        return loginView
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         clickEvent()
+        setLoginView()
     }
 
-    fun clickEvent(){
+    fun clickEvent() {
         binding.buttonLogin.setOnClickListener {
             findNavController().navigate(R.id.action_loginFragment_to_frameFragment)
         }
@@ -87,6 +46,52 @@ class LoginFragment : Fragment() {
         binding.buttonSignup.setOnClickListener {
             findNavController().navigate(R.id.action_loginFragment_to_signupFirstFragment)
         }
+    }
+
+    fun setLoginView() {
+
+        val edittextInputEmail = binding.edittextLoginEmail
+        val edittextInputPassword = binding.edittextLoginPassword
+        val buttonDeleteEmail = binding.buttonDeleteEmail
+        val buttonDeletePassword = binding.buttonDeletePassword
+
+        binding.edittextLoginEmail.addTextChangedListener(object : TextWatcher {
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (edittextInputEmail.text.toString().length > 0)
+                    buttonDeleteEmail.isVisible = true
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+        })
+        binding.edittextLoginPassword.addTextChangedListener(object : TextWatcher {
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (edittextInputPassword.text.toString().length > 0)
+                    buttonDeletePassword.isVisible = true
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+        })
+
+        binding.buttonDeleteEmail.setOnClickListener {
+            edittextInputEmail.setText("")
+            buttonDeleteEmail.isVisible = false
+        }
+
+        binding.buttonDeletePassword.setOnClickListener {
+            edittextInputPassword.setText("")
+            buttonDeletePassword.isVisible = false
+        }
+
     }
 
 
