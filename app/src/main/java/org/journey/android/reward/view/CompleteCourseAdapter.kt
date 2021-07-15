d2866b3ac11817cb52b75ad29b830fb6ca68618e
teagh82 <teagh8237@gmail.com>
2021-07-15 19:04:30 +0900

    [feat] #180 Reward Server

--- app/src/main/java/org/journey/android/reward/view/CompleteCourseAdapter.kt
+++ app/src/main/java/org/journey/android/reward/view/CompleteCourseAdapter.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import org.journey.android.R
 import org.journey.android.databinding.ItemRewardCourseBinding
 import org.journey.android.reward.dto.CompleteCourseData
 import org.journey.android.reward.util.CourseDiffCallback
@@ -34,6 +35,25 @@ class CompleteCourseAdapter :
             binding.textviewCourseTerm.text = completeCourseData.courseDate
             binding.textviewCourseSubject.text = completeCourseData.courseName
             binding.textviewCourseDate.text = completeCourseData.courseComplete
+
+            when(completeCourseData.property){
+                0 -> {
+                    binding.circleimageReward.setImageResource(R.drawable.course_health)
+                    binding.constraintlayoutRewardCourse.setBackgroundResource(R.drawable.library_round_health)
+                }
+                1 -> {
+                    binding.circleimageReward.setImageResource(R.drawable.course_mamoey)
+                    binding.constraintlayoutRewardCourse.setBackgroundResource(R.drawable.library_round_memory)
+                }
+                2 -> {
+                    binding.circleimageReward.setImageResource(R.drawable.course_detect)
+                    binding.constraintlayoutRewardCourse.setBackgroundResource(R.drawable.library_round_detect)
+                }
+                3 -> {
+                    binding.circleimageReward.setImageResource(R.drawable.course_challenge)
+                    binding.constraintlayoutRewardCourse.setBackgroundResource(R.drawable.library_round_challenge)
+                }
+            }
         }
     }
 
