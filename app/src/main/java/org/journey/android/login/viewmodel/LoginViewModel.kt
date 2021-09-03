deea86b62837487135266fea31a8f36fbc4e7a2e
jooyae <jooyae3970@naver.com>
2021-09-03 13:41:46 +0900

    [feat] #262 Mohaeng Login View

--- app/src/main/java/org/journey/android/login/viewmodel/LoginViewModel.kt
+++ app/src/main/java/org/journey/android/login/viewmodel/LoginViewModel.kt
@@ -1,8 +1,12 @@
 package org.journey.android.login.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import dagger.hilt.android.lifecycle.HiltViewModel
+import org.journey.android.base.BaseViewModel
 import org.journey.android.login.repository.LoginRepository
 import javax.inject.Inject
 
-class LoginViewModel @Inject constructor(
-    private val repository : LoginRepository
-)
+class LoginViewModel @Inject constructor (
+) : BaseViewModel()    {
+}
---
+++ app/src/main/java/org/journey/android/splash/OnboardingViewModel.kt
@@ -0,0 +1,31 @@
+package org.journey.android.splash
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import dagger.hilt.android.lifecycle.HiltViewModel
+import org.journey.android.base.BaseViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class OnboardingViewModel @Inject constructor() :BaseViewModel() {
+    private val _accessToken = MutableLiveData<String>()
+    private val _kakaoLogin = MutableLiveData<Boolean>()
+    private val _googleLogin = MutableLiveData<Boolean>()
+
+    val kakaoLogin: LiveData<Boolean>
+        get() = _kakaoLogin
+
+    val googleLogin : LiveData<Boolean>
+        get() = _googleLogin
+
+    val accessToken: LiveData<String>
+        get() = _accessToken
+
+    private val _refreshToken = MutableLiveData<String>()
+    val refreshToken: LiveData<String>
+        get() = _refreshToken
+
+    private val _loginSuccess = MutableLiveData<String>()
+    val loginSuccess: LiveData<String>
+        get() = _loginSuccess
+}
