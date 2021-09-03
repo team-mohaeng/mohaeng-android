d73d64082596c68937407142dfd4f4a7cd2568af
jooyae <jooyae3970@naver.com>
2021-09-03 15:24:20 +0900

    [feat] #264 Login With User Email

---
+++ app/src/main/java/org/journey/android/splash/ui/EmailLoginFragment.kt
@@ -0,0 +1,26 @@
+package org.journey.android.splash.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+import org.journey.android.databinding.FragmentEmailLoginBinding
+import org.journey.android.util.AutoClearedValue
+
+@AndroidEntryPoint
+class EmailLoginFragment : Fragment() {
+    private var binding by AutoClearedValue<FragmentEmailLoginBinding>()
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentEmailLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
--- app/src/main/java/org/journey/android/splash/ui/OnboardingFirstFragment.kt
+++ app/src/main/java/org/journey/android/splash/ui/OnboardingFirstFragment.kt
@@ -2,9 +2,11 @@ package org.journey.android.splash.ui
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import dagger.hilt.android.AndroidEntryPoint
 import org.journey.android.base.BaseFragment
 import org.journey.android.databinding.FragmentOnboardingFirstBinding
 
+@AndroidEntryPoint
 class OnboardingFirstFragment : BaseFragment<FragmentOnboardingFirstBinding>() {
     override fun getFragmentBinding(
         inflater: LayoutInflater,
