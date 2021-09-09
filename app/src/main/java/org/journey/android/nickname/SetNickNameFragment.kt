1ed25bdbb00850e7d7207934e6041396a9d88cfb
jooyae <jooyae3970@naver.com>
2021-09-09 11:12:13 +0900

    [feat] #284 Service Agreement

--- app/src/main/java/org/journey/android/nickname/SetNickNameFragment.kt
+++ app/src/main/java/org/journey/android/nickname/SetNickNameFragment.kt
@@ -1,24 +1,23 @@
-package org.journey.android
+package org.journey.android.nickname
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import org.journey.android.databinding.FragmentEditNickNameBinding
+import org.journey.android.databinding.FragmentSetNickNameBinding
 import org.journey.android.util.AutoClearedValue
 
-class EditNickNameFragment : Fragment() {
-    private var binding by AutoClearedValue<FragmentEditNickNameBinding>()
+class SetNickNameFragment : Fragment() {
+    private var binding by AutoClearedValue<FragmentSetNickNameBinding>()
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentEditNickNameBinding.inflate(inflater,container,false)
+        binding = FragmentSetNickNameBinding.inflate(inflater,container,false)
         return binding.root
     }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
     }
--- app/src/main/java/org/journey/android/splash/ui/ServiceAgreeFragment.kt
+++ app/src/main/java/org/journey/android/splash/ui/ServiceAgreeFragment.kt
@@ -1,21 +1,21 @@
-package org.journey.android
+package org.journey.android.splash.ui
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import org.journey.android.databinding.FragmentSetNickNameBinding
+import org.journey.android.databinding.FragmentServiceAgreeBinding
 import org.journey.android.util.AutoClearedValue
 
-class SetNickNameFragment : Fragment() {
-    private var binding by AutoClearedValue<FragmentSetNickNameBinding>()
+class ServiceAgreeFragment : Fragment() {
+    private var binding by AutoClearedValue<FragmentServiceAgreeBinding>()
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentSetNickNameBinding.inflate(inflater,container,false)
+        binding = FragmentServiceAgreeBinding.inflate(inflater, container,false)
         return binding.root
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
