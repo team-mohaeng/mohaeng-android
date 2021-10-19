fdad6036e420c51ebd69e9f3b7137f524454f7f5
jooyae <jooyae3970@naver.com>
2021-10-19 13:18:08 +0900

    [feat] OnboardingFifthFragment

---
+++ app/src/main/java/org/journey/android/OnboardingFifthFragment.kt
@@ -0,0 +1,26 @@
+package org.journey.android
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import org.journey.android.databinding.FragmentOnboardingFifthBinding
+import org.journey.android.util.AutoClearedValue
+
+class OnboardingFifthFragment : Fragment() {
+    private var binding by AutoClearedValue<FragmentOnboardingFifthBinding>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentOnboardingFifthBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
