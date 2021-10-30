eb428b848f2533ca7564f4b99db61936c2283e36
jooyae <jooyae3970@naver.com>
2021-10-30 15:51:23 +0900

    Onboarding Spannable Extension

--- app/src/main/java/org/journey/android/onboarding/ui/fragment/OnboardingSecondFragment.kt
+++ app/src/main/java/org/journey/android/onboarding/ui/fragment/OnboardingSecondFragment.kt
@@ -1,16 +1,24 @@
 package org.journey.android.onboarding.ui.fragment
 
 import android.os.Bundle
+import android.text.Spannable
+import android.text.style.ForegroundColorSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.core.text.toSpannable
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.findNavController
+import dagger.hilt.android.AndroidEntryPoint
+import org.journey.android.R
 import org.journey.android.databinding.FragmentOnboardingSecondBinding
 import org.journey.android.onboarding.ui.adapter.OnboardingCourseAdapter
 import org.journey.android.onboarding.viewmodel.OnboardingViewModel
 import org.journey.android.util.AutoClearedValue
 
+@AndroidEntryPoint
 class OnboardingSecondFragment : Fragment() {
     private var binding by AutoClearedValue<FragmentOnboardingSecondBinding>()
     private val viewModel by viewModels<OnboardingViewModel>()
@@ -33,12 +41,17 @@ class OnboardingSecondFragment : Fragment() {
     private fun fetchCourseRecyclerview(){
         binding.recyclerviewChooseCourse.apply{
             isNestedScrollingEnabled = false
-            adapter = OnboardingCourseAdapter()
+            adapter = OnboardingCourseAdapter(object : OnboardingCourseAdapter.CourseSelectListener{
+                override fun onClick() {
+                    findNavController().navigate(R.id.action_onboardingSecondFragment_to_onboardingThirdFragment)
+                }
+
+
+            } )
             viewModel.selectCourse.observe(viewLifecycleOwner){
                 (adapter as OnboardingCourseAdapter).selectCourse = it.toMutableList()
             }
 
         }
     }
-
 }
