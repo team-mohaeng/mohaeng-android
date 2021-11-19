93cb2f85e132f0b93cddc2b2cb07b12ee09fefb8
jooyae <jooyae3970@naver.com>
2021-11-19 14:35:21 +0900

    [fix] User Current Character & Skin

--- app/src/main/java/org/journey/android/character/viewmodel/CharacterViewModel.kt
+++ app/src/main/java/org/journey/android/character/viewmodel/CharacterViewModel.kt
@@ -1,11 +1,16 @@
 package org.journey.android.character.viewmodel
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.LiveDataScope
 import androidx.lifecycle.MutableLiveData
 import dagger.hilt.android.lifecycle.HiltViewModel
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.schedulers.Schedulers
 import org.journey.android.R
 import org.journey.android.base.BaseViewModel
 import org.journey.android.character.controller.CharacterController
+import org.journey.android.character.data.dto.CurrentCharacterDTO
+import org.journey.android.character.data.dto.CurrentSkinDTO
 import org.journey.android.character.data.entity.MohaengCharacterEntity
 import org.journey.android.character.data.entity.MohaengCharacterOptionEntity
 import javax.inject.Inject
@@ -21,6 +26,41 @@ class CharacterViewModel @Inject constructor(
     private val _optionList = MutableLiveData<List<MohaengCharacterOptionEntity>>()
     val optionList: LiveData<List<MohaengCharacterOptionEntity>>
         get() = _optionList
+
+    private val _currentUserCharacter = MutableLiveData<CurrentCharacterDTO>()
+    val currentUserCharacter : LiveData<CurrentCharacterDTO>
+        get() = _currentUserCharacter
+
+    private val _currentUserSkin = MutableLiveData<CurrentSkinDTO>()
+    val currentUserSkin : LiveData<CurrentSkinDTO>
+        get() = _currentUserSkin
+
+    fun loadUserCurrentCharacter(){
+        addDisposable(
+            characterController.getCharacter(client = "aos")
+                .observeOn(Schedulers.io())
+                .subscribeOn(AndroidSchedulers.mainThread())
+                .subscribe({ response ->
+                           _currentUserCharacter.postValue(response.dataDTO.currentCharacterDTO)
+                },{
+                    it.printStackTrace()
+                })
+        )
+    }
+
+    fun loadUserCurrentSkin(){
+        addDisposable(
+            characterController.getCharacter(client = "aos")
+                .observeOn(Schedulers.io())
+                .subscribeOn(AndroidSchedulers.mainThread())
+                .subscribe({ response ->
+                    _currentUserSkin.postValue(response.dataDTO.currentSkinDTO)
+
+                },{
+                    it.printStackTrace()
+                })
+        )
+    }
 
     init {
         fetchCharacterList()
