2020a8f283656792fcd04c50cba282fd58b5a27e
teagh82 <teagh8237@gmail.com>
2022-05-29 14:07:30 +0900

    [fix] #454 Change Server api address

--- app/src/main/java/org/journey/android/di/NetworkModule.kt
+++ app/src/main/java/org/journey/android/di/NetworkModule.kt
@@ -63,7 +63,7 @@ object NetworkModule {
     @UnAuthRetrofit
     fun provideAuthRetrofit(@UnAuthOkHttp okHttpClient: OkHttpClient): Retrofit {
         return Retrofit.Builder()
-            .baseUrl("http://3.34.194.201:5000/")
+            .baseUrl("http://mohaeng.me:5000/")
             .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
@@ -75,7 +75,7 @@ object NetworkModule {
     @AuthRetrofit
     fun provideRetrofit(@AuthOkHttp okHttpClient: OkHttpClient): Retrofit =
         Retrofit.Builder()
-            .baseUrl("http://3.34.194.201:5000/")
+            .baseUrl("http://mohaeng.me:5000/")
             .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
